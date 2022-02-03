c0c1acb1c691743c11dc7d0ef43eb5bf03892a92
Justus Onuoha <valzey87@gmail.com>
2022-02-04 00:41:35 +0100

    create modal component

--- src/theme/dark.ts
+++ src/theme/dark.ts
@@ -1,19 +1,16 @@
 import { DefaultTheme } from "styled-components";
 import { dark as darkToggle } from "../components/Toggle/theme";
 import { dark as darkNav } from "../widgets/Menu/theme";
-import { dark as darkModal } from "../widgets/Modal/theme";
 import base from "./base";
 import { darkColors } from "./colors";
 
 const darkTheme: DefaultTheme = {
   ...base,
   isDark: true,
-  alert: {},
   colors: darkColors,
   toggle: darkToggle,
   nav: darkNav,
-  modal: darkModal,
-  radio: {},
+  radio: {} as any,
 };
 
 export default darkTheme;
--- src/theme/light.ts
+++ src/theme/light.ts
@@ -1,19 +1,16 @@
 import { DefaultTheme } from "styled-components";
 import { light as lightToggle } from "../components/Toggle/theme";
 import { light as lightNav } from "../widgets/Menu/theme";
-import { light as lightModal } from "../widgets/Modal/theme";
 import base from "./base";
 import { lightColors } from "./colors";
 
 const lightTheme: DefaultTheme = {
   ...base,
   isDark: false,
-  alert: {},
   colors: lightColors,
   toggle: lightToggle,
   nav: lightNav,
-  modal: lightModal,
-  radio: {},
+  radio: {} as any,
 };
 
 export default lightTheme;
