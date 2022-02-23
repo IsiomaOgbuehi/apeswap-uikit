5a288bd9df80b710958a91676c19af947eb41deb
Justus Onuoha <valzey87@gmail.com>
2022-02-23 14:04:56 +0100

    export menu context

--- src/widgets/Menu/index.ts
+++ src/widgets/Menu/index.ts
@@ -2,5 +2,7 @@ export { default as Menu } from "./Menu";
 export { default as MenuBody } from "./MenuBody";
 export { default as MenuLink } from "./MenuLink";
 export { default as MenuFooter } from "./MenuFooter";
+export { default as MenuContext } from "./MenuContext";
+export { MenuContextProvider } from "./MenuContext";
 
 export type { MenuContextProps, MenuItem, MenuLinkProps } from "./types";
